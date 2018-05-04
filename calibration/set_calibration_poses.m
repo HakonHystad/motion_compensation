aaebd2eae5040184a86becf6426aa3a3c8d6fa8b
HakonHystad <hakonhystad@gmail.com>
2018-05-04 16:00:11 +0200

    changed for kukavarproxy

--- calibration/set_calibration_poses.m
+++ calibration/set_calibration_poses.m
@@ -276,10 +276,11 @@ end
 % number of images/poses to take
 fprintf(fid, '%d\n', length(final_poses) );
 % each pose as X Y Z A B C
+func = @(T) [rotz(-90) [0 0 0]'; 0 0 0 1 ]*T;
+final_poses = cellfun( func, final_poses, 'UniformOutput', false );
 
 for i=1:length(final_poses)
     ABC = rad2deg( rotm2eul( final_poses{i}(1:3,1:3), 'ZYX' ) );
-    ABC(1) = ABC(1) - 90;% bodge because of kukavarproxy nullframe
     fprintf(fid, '%.4f %.4f %.4f %.4f %.4f %.4f\n', final_poses{i}(1:3,4), ABC );
 end
 
